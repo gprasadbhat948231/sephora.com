c5737e4b3056946b7ac9b3ae576c29239a91a6f5
gprasadbhat948231 <gprasadbhat948231@gmail.com>
2022-12-15 08:45:29 +0530

    added webviteals

--- nemow/src/index.js
+++ nemow/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
