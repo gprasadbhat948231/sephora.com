1a11671f359eec156f13427c82885a1b10fb575b
Muhammed sadique <mhdsadique8@gmail.com>
2022-12-15 19:07:40 +0530

    day3

---
+++ nemow/src/Components/fountation/loading.jsx
@@ -0,0 +1,69 @@
+import { Box, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+
+
+function LoadingIndicator() {
+    return(<>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box><Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box><Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+<Box padding='6' boxShadow='lg' bg='#CBD5E0'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='6' noOfLines={4} spacing='8' skeletonHeight='4' />
+
+</Box>
+    </>
+)
+}
+
+export default LoadingIndicator;
