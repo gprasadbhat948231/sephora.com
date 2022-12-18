3718bd455ba64e54cf1d329b27d01c51216dbebe
Ritik Singh <delhi8260@gmail.com>
2022-12-18 20:17:29 +0530

    final work done

--- nemow/src/App.js
+++ nemow/src/App.js
@@ -3,14 +3,6 @@ import CartPage, { Wishlist } from './Components/CartPage/CartPage';
 import LoadingIndicator from './Components/fountation/loading';
 import Navbar from './Components/Navbar';
 import EyeCare, { LoadingComponent } from './Pages/EyeCare';
-
-import Home from './Pages/Home';
-
-function App() {
-  return (
-    <div>
-
-import Navbar from './Components/Navbar';
 import Footer from "./Pages/Footer";
 import AllRoutes from './Pages/AllRoutes';
 function App() {
@@ -19,7 +11,6 @@ function App() {
       <Navbar/>
       <AllRoutes/>
       <Footer/>
-
     </div>
   )
 }
