faf0a148cc7d54d5db6f2290d4dd51b256806c0b
Ritik Singh <delhi8260@gmail.com>
2022-12-19 10:52:18 +0530

    Loading skeleton added

--- nemow/src/Pages/New.jsx
+++ nemow/src/Pages/New.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import '../Components/NewPage/New.css'
 import '../index.css'
 import Card from "../Components/NewPage/Card.jsx";
-import {Box,Heading,Text,Grid,useToast} from "@chakra-ui/react";
+import { Box, Heading, Text, Grid, useToast } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import { addtocart_Eyecare, addtowishlist_Eyecare, cartlistGetdata, Remove_from_Wishlist, wishlistGetdata } from "../HOC/EyecareRedux/Actions";
 import { LoadingComponent } from "./EyeCare";
 const New = () => {
-    const {Wishlist,CartList}=useSelector((state)=>state.CartandWishReducer)
-    const dispatch=useDispatch()
+    const { Wishlist, CartList } = useSelector((state) => state.CartandWishReducer)
+    const dispatch = useDispatch()
     const [products, setProducts] = useState([]);
 
     const toast = useToast();
@@ -60,41 +60,41 @@ const New = () => {
     };
 
     // to know wish list //
-  const ToKnowWishlist = (product) => {
+    const ToKnowWishlist = (product) => {
         if (Wishlist.find((item) => product.id === item.id)) {
             return true;
         }
         return false;
     };
 
-     // to know cart list //
-  const ToknowCartList = (Cartitem) => {
-    if (CartList.find((item) => Cartitem.id === item.id)) {
-      return true;
-    }
-    return false;
-  };
+    // to know cart list //
+    const ToknowCartList = (Cartitem) => {
+        if (CartList.find((item) => Cartitem.id === item.id)) {
+            return true;
+        }
+        return false;
+    };
 
     const AddedtoCartList = (Cartitem) => {
         if (!CartList.find((item) => Cartitem.id === item.id)) {
-          dispatch(addtocart_Eyecare(Cartitem));
-          toast({
-            title: "Cartlist.",
-            description: "Item Added To Cartlist Succesfully.",
-            status: "success",
-            duration: 4000,
-            isClosable: true,
-          });
+            dispatch(addtocart_Eyecare(Cartitem));
+            toast({
+                title: "Cartlist.",
+                description: "Item Added To Cartlist Succesfully.",
+                status: "success",
+                duration: 4000,
+                isClosable: true,
+            });
         }
-      };
+    };
 
-    const breakpoints = { sm: '30em', md: '48em', lg: '62em', xl: '80em', '2xl': '96em' }
+    // const breakpoints = { sm: '30em', md: '48em', lg: '62em', xl: '80em', '2xl': '96em' }
     // brand, imagePath, sellingPriceRange: {min: 440, max: 440}, name: "Color Hit Nail Polish - L153 Blue Suede Shoes", brand, id, discountRange: {min: 0, max: 0}
     return (
         <div style={{ display: 'flex', width: '85%', margin: 'auto', marginTop: '10vh' }}>
             <div className="others-pages-div" style={{ width: '20%', marginRight: '2vw' }}>
                 <Text color='#666' fontSize='13' mb='5'>New</Text>
-                <Box borderTop='2px solid' borderBottom='2px solid' pt='4' pb='4' lineHeight='1.7' fontSize={{ base: '6', md: '10', lg: '15' }}>
+                <Box borderTop='2px solid' borderBottom='2px solid' pt='4' pb='4' lineHeight='1.7' fontSize={{ base: '8', md: '10', lg: '15' }}>
                     <Heading fontSize='16px' mb='2'>New</Heading>
                     <Text className="sub-pages" onClick={() => handlePageChange(just_dropped_api)}>Just Dropped</Text>
                     <Text className="sub-pages" onClick={() => handlePageChange(makup_api)}>Makeup</Text>
@@ -108,17 +108,18 @@ const New = () => {
 
             <Box w="85%" borderLeft='1px solid grey' pl='5'>
                 <Text align='center' fontSize='32px' mb='8' fontFamily='georgia, times, serif'>New</Text>
-                <Grid templateColumns="repeat(4, 1fr)" rowGap={5} columnGap={2}>
-                        {/* {products.length===0?<LoadingComponent/>:
-                        products.map((product) => (
-                            <Card key={product.id} product={product} AddedtoWishlist={AddedtoWishlist} AddedtoCartList={AddedtoCartList} ToKnowWishlist={ToKnowWishlist} ToknowCartList={ToknowCartList}/>
-                        ))
-                        } */}
+                {products.length === 0 ? <LoadingComponent /> :
+                    <Grid templateColumns={{
+                        lg: "repeat(4, 1fr)",
+                        xl: "repeat(4, 1fr)",
+                        sm: "repeat(2, 1fr)",
+                        md: "repeat(3, 1fr)",
+                    }} rowGap={5} columnGap={2}>
                         {products.map((product) => (
-                            <Card key={product.id} product={product} AddedtoWishlist={AddedtoWishlist} AddedtoCartList={AddedtoCartList} ToKnowWishlist={ToKnowWishlist} ToknowCartList={ToknowCartList}/>
+                            <Card key={product.id} product={product} AddedtoWishlist={AddedtoWishlist} AddedtoCartList={AddedtoCartList} ToKnowWishlist={ToKnowWishlist} ToknowCartList={ToknowCartList} />
                         ))
                         }
-                </Grid>
+                    </Grid>}
             </Box>
         </div>
     )
